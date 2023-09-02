044277e8abf51480be2ddf460cd9524977e08425
ymkmy98 <ymkmy98@gmail.com>
2023-09-02 22:58:30 +0900

    mvに3d追加、背景変化js微修正

--- js/function.js
+++ js/function.js
@@ -1,6 +1,6 @@
 $(function () {
   // loading
-  var webStorage = function () {
+  let webStorage = function () {
     if (sessionStorage.getItem("access")) {
       $("#splash").fadeOut(function () {
         $(".splashbg").addClass("pageAnime");
@@ -22,14 +22,12 @@ $(function () {
   };
   webStorage();
 
-  // mouse stolker
+  // mouse stalker
   const stalker = $("#mouse");
 
-  $(document).on("mousemove", function (e) {
-    $(document).on("mousemove", function (e) {
-      stalker.css({
-        transform: "translate(" + e.clientX + "px, " + e.clientY + "px)",
-      });
+  window.addEventListener("mousemove", function (e) {
+    stalker.css({
+      transform: "translate(" + e.clientX + "px, " + e.clientY + "px)",
     });
   });
 
@@ -105,7 +103,7 @@ $(function () {
       $("#btn span, #mouse").css({
         "background-color": "#000",
       });
-
+      document.getElementById("mv_canvas").style.opacity = 0;
       $(".mv__scroll").css({
         opacity: "0",
       });
@@ -120,14 +118,14 @@ $(function () {
       $("#btn span, #mouse").css({
         "background-color": "#fff5f0",
       });
-
+      document.getElementById("mv_canvas").style.opacity = 0.6;
       $(".mv__scroll").css({
         opacity: "1",
       });
     }
   });
 
-  $(window).scroll(function () {
+  window.addEventListener("scroll", function () {
     // section number
     if (
       $(window).scrollTop() > $("#about").offset().top - 500 &&
@@ -210,22 +208,64 @@ $(function () {
     }
   });
 
-  //works hover
+  // works hover
   if (window.matchMedia("(min-width: 1024px)").matches) {
-    $(".work__item").hover(
-      function () {
-        const target = $(this).data("target");
+    // $(".work__item").hover(
+    //   function () {
+    //     const target = $(this).data("target");
 
-        $(".work__item, #workScroll").not(this).stop().css("opacity", "0.2");
-        $(".work__huge").addClass("workBgActive");
-        $(target).addClass("workBgActive");
-      },
-      function () {
-        $(".work__item, #workScroll").not(this).stop().css("opacity", "1");
-        $(".work__huge").removeClass("workBgActive");
-        $(".work__bg").removeClass("workBgActive");
-      }
-    );
+    //     $(".work__item, #workScroll").not(this).stop().css("opacity", "0.2");
+    //     $(".work__huge").addClass("workBgActive");
+    //     $(target).addClass("workBgActive");
+    //   },
+    //   function () {
+    //     $(".work__item, #workScroll").not(this).stop().css("opacity", "1");
+    //     $(".work__huge").removeClass("workBgActive");
+    //     $(".work__bg").removeClass("workBgActive");
+    //   }
+    // );
+    const workItems = document.querySelectorAll(".work__item");
+    // const workScroll = document.querySelector("#workScroll");
+    const workHuge = document.querySelectorAll(".work__huge");
+    const workBg = document.querySelectorAll(".work__bg");
+
+    workItems.forEach((workItem) => {
+      workItem.addEventListener("mouseenter", function () {
+        const target = this.getAttribute("data-target");
+
+        workItems.forEach((item) => {
+          if (item !== this) {
+            item.style.opacity = "0.2";
+          }
+        });
+
+        // workScroll.style.opacity = "0.2";
+
+        workHuge.forEach((huge) => {
+          huge.classList.add("workBgActive");
+        });
+
+        document.querySelector(target).classList.add("workBgActive");
+      });
+
+      workItem.addEventListener("mouseleave", function () {
+        workItems.forEach((item) => {
+          if (item !== this) {
+            item.style.opacity = "1";
+          }
+        });
+
+        // workScroll.style.opacity = "1";
+
+        workHuge.forEach((huge) => {
+          huge.classList.remove("workBgActive");
+        });
+
+        workBg.forEach((bg) => {
+          bg.classList.remove("workBgActive");
+        });
+      });
+    });
   }
 
   // footer message
@@ -315,7 +355,7 @@ const swiperTxt = new Swiper(".swiper-txt", {
   },
 });
 
-//svg
+// svg
 window.addEventListener("load", function () {
   deSVG(
     ".header__logo, .mv__img, .stroke__Fimg, .stroke__Bimg, .work__img",
@@ -323,87 +363,206 @@ window.addEventListener("load", function () {
   );
 });
 
+// mv 3d
 import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-// three.js
-window.addEventListener("DOMContentLoaded", init);
+// window.addEventListener("DOMContentLoaded", mvThree);
 
-async function init() {
-  const width = window.innerWidth;
-  const height = window.innerHeight * 0.8;
+// Three.jsの実行状態を管理するフラグ
+let isMvThreeRunning = false;
 
-  // レンダラーを作成
-  const canvasElement = document.querySelector("#mv_canvas");
-  const renderer = new THREE.WebGLRenderer({
-    canvas: canvasElement,
+let animationFrameID,
+  scene,
+  camera,
+  light,
+  ambientLight,
+  model,
+  particles,
+  renderer = null;
+
+const canvas = document.querySelector("#mv_canvas");
+
+// #my_canvasを監視、画面内にあるときのみmvThreeを実行
+const options = {
+  root: null,
+  rootMargin: "0%",
+  threshold: 0,
+};
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // isMvThreeRunningがfalseのとき（Three.jsが実行されていないとき）に実行
+      if (!isMvThreeRunning) {
+        mvThree();
+      }
+    } else {
+      // 画面外に出たら監視を停止
+      // observer.unobserve(canvas);
+      if (isMvThreeRunning) {
+        // Three.jsが実行されていたら停止する
+        stopMvThree();
+      }
+    }
+  });
+}, options);
+
+observer.observe(canvas);
+
+// three.js 実行関数
+async function mvThree() {
+  // フラグをtrueにする
+  isMvThreeRunning = true;
+  // console.log("実行");
+
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+
+  // renderer
+  renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
   });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(width, height);
   renderer.setClearColor(0x000000, 0);
 
-  // シーンを作成
-  const scene = new THREE.Scene();
+  // scene
+  scene = new THREE.Scene();
 
-  // カメラを作成
-  const camera = new THREE.PerspectiveCamera(45, width / height, 0.001, 10000);
-  // カメラの初期座標を設定
-  camera.position.set(2, 0, 0);
+  // camera
+  camera = new THREE.PerspectiveCamera(45, width / height, 0.001, 10000);
+  camera.position.set(0.1, 0.05, 0.5);
   scene.add(camera);
 
-  // カメラコントローラーを作成
-  const controls = new OrbitControls(camera, canvasElement);
-  controls.target.set(0, 0, 0);
-  controls.update();
-
-  // 平行光源を作成
-  // // 上から照らす
-  // const directionalLight = new THREE.DirectionalLight(0xffffff);
-  // directionalLight.position.set(1, 1, 1);
-  // scene.add(directionalLight);
-
-  // // 横からテラス
-  // const directionalLight2 = new THREE.DirectionalLight(0xffffff);
-  // directionalLight2.position.set(1, 0, 1);
-  // scene.add(directionalLight2);
-  const light = new THREE.DirectionalLight("#fff5f0", 10);
-  light.position.set(1, 0.5, 0.5);
+  // light
+  light = new THREE.DirectionalLight("#fff5f0", 1);
+  light.position.set(1, 1, 1);
   scene.add(light);
 
-  const ambientLight = new THREE.AmbientLight();
+  ambientLight = new THREE.AmbientLight();
   ambientLight.color = new THREE.Color(0xffffff);
   ambientLight.intensity = 0.5;
   ambientLight.position.x = 2;
   scene.add(ambientLight);
 
-  // GLTF形式のモデルデータを読み込む
+  // model by gltf
   const loader = new GLTFLoader();
-  // GLTFファイルのパスを指定
   const objects = await loader.loadAsync("./images/3d/mv_huge_3d.glb");
-  // 読み込み後に3D空間に追加
-  const model = objects.scene;
-  // model.position.set(0, 0.1, 0.5);
-  model.scale.set(4, 4, 4); // 大きさ30倍に拡大
+  model = objects.scene;
+
+  model.scale.set(0.68, 0.68, 0.7);
+  model.position.set(0.1, 0.045, 0);
+  model.rotation.set(0, -1.6, 0);
   scene.add(model);
+
+  // particle
+  const particlesGeometry = new THREE.BufferGeometry();
+  const particlesCount = 700;
+
+  const positionArray = new Float32Array(particlesCount * 3);
+
+  for (let i = 0; i < particlesCount * 3; i++) {
+    positionArray[i] = (Math.random() - 0.5) * 10;
+  }
+
+  particlesGeometry.setAttribute(
+    "position",
+    new THREE.BufferAttribute(positionArray, 3)
+  );
+
+  // particles material
+  const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.025,
+    sizeAttenuation: true,
+    color: "#fff5f0",
+  });
+
+  particles = new THREE.Points(particlesGeometry, particlesMaterial);
+  scene.add(particles);
 
   //ブラウザのリサイズに対応
   window.addEventListener("resize", () => {
-    sizes.width = window.innerWidth;
-    sizes.height = window.innerHeight;
+    width = window.innerWidth;
+    height = window.innerHeight;
 
-    camera.aspect = sizes.width / sizes.height;
+    camera.aspect = width / height;
     camera.updateProjectionMatrix();
 
-    renderer.setSize(sizes.width, sizes.height);
+    renderer.setSize(width, height);
     renderer.setPixelRatio(window.devicePixelRatio);
   });
 
-  tick();
+  // カーソルの位置を取得する
+  const cursor = {
+    x: 0,
+    y: 0,
+  };
 
-  // 毎フレーム時に実行されるループイベントです
-  function tick() {
-    // レンダリング
-    renderer.render(scene, camera);
-    requestAnimationFrame(tick);
-  }
+  const cursorEl = document.querySelector("#mv");
+
+  window.addEventListener("mousemove", (e) => {
+    cursor.x = e.clientX / width - 0.5;
+    cursor.y = e.clientY / height - 0.5;
+
+    let canvasRect = cursorEl.getBoundingClientRect();
+
+    // カーソルが要素内にあるかどうかを判定
+    if (
+      cursor.x >= canvasRect.left / window.innerWidth - 0.5 &&
+      cursor.x <= canvasRect.right / window.innerWidth - 0.5 &&
+      cursor.y >= canvasRect.top / window.innerHeight - 0.5 &&
+      cursor.y <= canvasRect.bottom / window.innerHeight - 0.5
+    ) {
+      cursorInside = true;
+    } else {
+      cursorInside = false;
+    }
+    // console.log(cursor.x, cursor.y);
+  });
+
+  // カーソルが画面外にいるかどうかのフラグ
+  let cursorInside = true;
+  cursorEl.addEventListener("mouseout", () => {
+    cursorInside = false;
+  });
+  cursorEl.addEventListener("mouseover", () => {
+    cursorInside = true;
+  });
+
+  // animation
+  const clock = new THREE.Clock();
+  const animate = () => {
+    if (isMvThreeRunning) {
+      // pcのfpsに合わせる
+      let getDeltaTime = clock.getDelta();
+
+      renderer.render(scene, camera);
+
+      // オブジェクトを常に回転させ続ける
+      model.rotation.x += 0.03 * getDeltaTime;
+      model.rotation.y += 0.03 * getDeltaTime;
+      model.rotation.z += 0.03 * getDeltaTime;
+
+      // カーソルを用いたカメラの制御（カーソルが画面内にあるときのみ）
+      if (cursorInside) {
+        camera.position.x += cursor.x * getDeltaTime * 0.03;
+        camera.position.y += -cursor.y * getDeltaTime * 0.03;
+      }
+
+      animationFrameID = requestAnimationFrame(animate);
+    }
+  };
+  animate();
+}
+
+// three.js 停止関数
+function stopMvThree() {
+  cancelAnimationFrame(animationFrameID);
+  scene = new THREE.Scene();
+  camera = null;
+  particles = null;
+  renderer.setSize(0, 0);
+  renderer.dispose();
+  renderer = null;
+  isMvThreeRunning = false;
+  // console.log("停止");
 }
